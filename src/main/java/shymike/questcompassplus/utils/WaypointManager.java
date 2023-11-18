c2517ddadb1ebe7391ff2e70390f7ea5898b8417
ImShyMike <122023566+ImShyMike@users.noreply.github.com>
2023-11-18 12:00:21 +0000

    waypoint createor
    added waypoint creator (journey map still required for it :sob:)

--- src/main/java/shymike/questcompassplus/utils/WaypointManager.java
+++ src/main/java/shymike/questcompassplus/utils/WaypointManager.java
@@ -13,11 +13,17 @@ public class WaypointManager {
 		
 		double distance = Math.round(DistanceCalculator.getDistance2D(playerPos.x, playerPos.z, x, z));
 		RenderUtils.setCoordinates(x,y,z);
-		RenderUtils.line1 = "Compass Position: " + x + " " + y + " " + z;
+		RenderUtils.line1 = "Compass Position: " + (int)x + " " + (int)y + " " + (int)z;
 		if (Config.chatFeedback) {
-	    	ChatUtils.send(Text.literal("Compass Position: x=" + x + ", y=" + y + ", z=" + z + ", distance=" + distance).styled(style -> style
-	    			.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, x+" "+y+" "+z))
-	    			.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to copy coordinates to clipboard!")))));
+			if (Config.waypointCopy) {
+					ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
+							.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/jm wpedit [name:\"Quest Location\", x:"+(int)Config.x+", y:"+(int)Config.y+", z:"+(int)Config.z+", dim:minecraft:overworld]"))
+							.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to make waypoint!")))));
+				} else if (Config.chatFeedback) {
+			    	ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
+			    			.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, (int)Config.x+" "+(int)Config.y+" "+(int)Config.z))
+			    			.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to copy coordinates to clipboard!")))));
+				}
 		}
 		
 		// TODO: actual waypoints
