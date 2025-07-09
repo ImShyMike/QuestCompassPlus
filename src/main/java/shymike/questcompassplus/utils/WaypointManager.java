f1bd2e9270121c49534613f9cfacea5bf6e7f7f8
RectangularObject <26125768+RectangularObject@users.noreply.github.com>
2025-07-09 08:34:26 -0400

    change command to support xaero's waypoints

--- src/main/java/shymike/questcompassplus/utils/WaypointManager.java
+++ src/main/java/shymike/questcompassplus/utils/WaypointManager.java
@@ -12,14 +12,15 @@ import java.util.Objects;
 public class WaypointManager {
     public static void create(MinecraftClient mc, double x, double y, double z) {
 		Vec3d playerPos = Objects.requireNonNull(mc.player).getPos();
-		
+		String dimension = Objects.requireNonNull(mc.world).getRegistryKey().getValue().toString();
+
 		double distance = Math.round(DistanceCalculator.getDistance2D(playerPos.x, playerPos.z, x, z));
 		RenderUtils.setCoordinates(x,y,z);
 		RenderUtils.line1 = "Compass Position: " + (int)x + " " + (int)y + " " + (int)z;
 		if (Config.chatFeedback) {
 			if (Config.waypointCopy) {
 					ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
-							.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/jm wpedit [name:\"Quest Location\", x:"+(int)Config.x+", y:"+(int)Config.y+", z:"+(int)Config.z+", dim:minecraft:overworld]"))
+							.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/xaero_waypoint_add:Compass:C:"+(int)Config.x+":"+(int)Config.y+":"+(int)Config.z+":4:false:0:Internal_dim%" + dimension.replace(":", "$") + "_waypoints"))
 							.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to make waypoint!")))));
 				} else if (Config.chatFeedback) {
 			    	ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
@@ -27,7 +28,7 @@ public class WaypointManager {
 			    			.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to copy coordinates to clipboard!")))));
 				}
 		}
-		
+
 		// TODO: actual waypoints
 	}
 }
