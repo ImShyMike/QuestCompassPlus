acefaa2abe26dac5e1df2b3fdeb21a752b46db6c
RectangularObject <26125768+RectangularObject@users.noreply.github.com>
2025-07-09 21:32:34 -0400

    add a bit of pizazz to the chat message

--- src/main/java/shymike/questcompassplus/utils/WaypointManager.java
+++ src/main/java/shymike/questcompassplus/utils/WaypointManager.java
@@ -19,11 +19,11 @@ public class WaypointManager {
 		RenderUtils.line1 = "Compass Position: " + (int)x + " " + (int)y + " " + (int)z;
 		if (Config.chatFeedback) {
 			if (Config.waypointCopy) {
-					ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
-							.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/xaero_waypoint_add:Compass:C:"+(int)Config.x+":"+(int)Config.y+":"+(int)Config.z+":4:false:0:Internal_dim%" + dimension.replace(":", "$") + "_waypoints"))
-							.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to make waypoint!")))));
+					ChatUtils.send(Text.literal("§3[QCP] §7Compass Position: §a(" + (int)Config.x + ", " + (int)Config.y + ", " + (int)Config.z + ") §7Distance: §2" + (int)distance + "§r").styled(style -> style
+							.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/xaero_waypoint_add:Compass:C:"+(int)Config.x+":"+(int)Config.y+":"+(int)Config.z+":3:false:0:Internal_dim%" + dimension.replace(":", "$") + "_waypoints"))
+							.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("§aClick to make waypoint!§r")))));
 				} else {
-			    	ChatUtils.send(Text.literal("Compass Position: x=" + (int)Config.x + ", y=" + (int)Config.y + ", z=" + (int)Config.z + ", distance=" + (int)distance).styled(style -> style
+			    	ChatUtils.send(Text.literal("§3[QCP] §7Compass Position: §a(" + (int)Config.x + ", " + (int)Config.y + ", " + (int)Config.z + ") §7Distance: §2" + (int)distance + "§r").styled(style -> style
 			    			.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, (int)Config.x+" "+(int)Config.y+" "+(int)Config.z))
 			    			.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to copy coordinates to clipboard!")))));
 				}
